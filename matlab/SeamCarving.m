5f88d977747a0020a99923e7a9807f61745b1b9c
Jasen Hall <jasen@cs.ucsb.edu>
2013-05-28 19:45:11 -0700

    Conversion still in process. Probably doesn't compile atm

--- matlab/SeamCarving.m
+++ matlab/SeamCarving.m
@@ -25,6 +25,7 @@ imshow(ReducedGrayImage);
 figure
 imshow(ReducedInputImage);
 end
+
 % calculating gradient for input image
 function [GradMean] = CalcGradient(inputImage)
     sobelOpx = [ 1 0 -1 ; 2  0  -2; 1 0 -1];
@@ -37,6 +38,7 @@ function [GradMean] = CalcGradient(inputImage)
     end
     GradMean = sum(Grad, 3)/dim;
 end
+
 %Remove Vertial Seam
 function [ReducedInputImage,ReducedGradMean, ReducedGrayImage] = RemoveVer(ReducedGradMean, ReducedInputImage, ReducedGrayImage)
 EnergyMapVer = CalcEnergyMapForward(ReducedGradMean, ReducedGrayImage);
@@ -45,6 +47,7 @@ ReducedInputImage = RemoveSeam(ReducedInputImage,SeamVer);
 ReducedGrayImage = RemoveSeam(ReducedGrayImage,SeamVer);
 ReducedGradMean = RemoveSeamGradient(ReducedGradMean, SeamVer);
 end
+
 % Remove Horizontal Seam. It uses same functions as vertical so we need to
 % transpose images before use those functions.
 function [ReducedInputImage,ReducedGradMean, ReducedGrayImage] = RemoveHor(ReducedGradMean, ReducedInputImage, ReducedGrayImage)
@@ -61,46 +64,53 @@ end
 %Calculating Forward energy map
 function [EnergyMap] = CalcEnergyMapForward(GradMean,ReducedGrayImage)
     [rows, cols] = size(GradMean);
-    for i = 1:1:rows
-        for j = 1:1:cols
-            if(i== 1)
-                EnergyMap(1,:) = GradMean(1,:);
-            else
-                if(j == 1)
-                    EnergyMap(i,j) = GradMean(i,j)+ min((EnergyMap(i-1,j) + costVertical(i,j,ReducedGrayImage)), (EnergyMap(i-1,j+1)+costRight(i,j,ReducedGrayImage)));
-                elseif(j == cols)
-                    EnergyMap(i,j) = GradMean(i,j)+ min((EnergyMap(i-1,j-1)+ costLeft(i,j,ReducedGrayImage)), (EnergyMap(i-1,j)+ costVertical(i,j,ReducedGrayImage)));
-                else
-                    EnergyMap(i,j) = GradMean(i,j)+ min(min((EnergyMap(i-1,j-1)+ costLeft(i,j,ReducedGrayImage)), (EnergyMap(i-1,j)+ costVertical(i,j,ReducedGrayImage))), (EnergyMap(i-1,j+1)+costRight(i,j,ReducedGrayImage)));
-                end
-                    
-            end
-        end
+  for i = 1:1:rows
+    for j = 1:1:cols
+      if(i== 1)
+        EnergyMap(1,:) = GradMean(1,:);
+      else if(j == 1)
+        EnergyMap(i,j) = GradMean(i,j)+ min((EnergyMap(i-1,j) +
+          costVertical(i,j,ReducedGrayImage)), (EnergyMap(i-1,j+1)+
+          costRight(i,j,ReducedGrayImage)));
+      elseif(j == cols)
+        EnergyMap(i,j) = GradMean(i,j)+ min((EnergyMap(i-1,j-1)+
+          costLeft(i,j,ReducedGrayImage)), (EnergyMap(i-1,j)+
+          costVertical(i,j,ReducedGrayImage)));
+      else
+        EnergyMap(i,j) = GradMean(i,j)+ min(min((EnergyMap(i-1,j-1)+
+          costLeft(i,j,ReducedGrayImage)), (EnergyMap(i-1,j)+
+          costVertical(i,j,ReducedGrayImage))), (EnergyMap(i-1,j+1)+
+          costRight(i,j,ReducedGrayImage)));
+      end
     end
+  end
 end
+
 %Additional cost functions for forward energy map
 function [value] = costLeft(i,j,ReducedGrayImage)
-[cols] = size(ReducedGrayImage,2);
-if(j==cols)
+  [cols] = size(ReducedGrayImage,2);
+  if(j==cols)
     value = abs(ReducedGrayImage(i-1,j)-ReducedGrayImage(i,j-1));
-else
+  else
     value = abs(ReducedGrayImage(i,j+1)-ReducedGrayImage(i,j-1))+abs(ReducedGrayImage(i-1,j)-ReducedGrayImage(i,j-1));
+  end
 end
-end
+
 function [value] = costRight(i,j,ReducedGrayImage)
-if(j==1)
+  if(j==1)
     value = abs(ReducedGrayImage(i-1,j)-ReducedGrayImage(i,j+1));
-else
+  else
     value = abs(ReducedGrayImage(i,j+1)-ReducedGrayImage(i,j-1))+abs(ReducedGrayImage(i-1,j)-ReducedGrayImage(i,j+1));
+  end
 end
-end
+
 function [value] = costVertical(i,j,ReducedGrayImage)
-[cols] = size(ReducedGrayImage,2);
-if(j==1 || j==cols)
+  [cols] = size(ReducedGrayImage,2);
+  if(j==1 || j==cols)
     value = 0;
-else
+  else
     value = abs(ReducedGrayImage(i,j+1)-ReducedGrayImage(i,j-1));
-end
+  end
 end
 
 %Finding Seam from Energy Map
@@ -124,6 +134,7 @@ for k = 1:1:rows
     end
 end
 end
+
 %Removing seam from image
 function [ReducedImage] = RemoveSeam(inputImage, Seam)
 [rows, cols, dim] = size(inputImage);
@@ -142,6 +153,7 @@ for k=1:1:dim
         
 end
 end
+
 %Remove Seam from gradient image. We add importance diffusion for gradient
 %image in this funciton so we calculated it seperately.
 function [ReducedGradMean] = RemoveSeamGradient(GradMean, Seam)
